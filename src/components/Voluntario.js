251c2522adb4b111c3dd0b68f944427b2463d461
Ever Zamora <ever.zamora@catolica.edu.sv>
2024-09-05 21:20:26 -0600

    Sonido  del tigre arreglado en voluntario

--- src/components/Voluntario.js
+++ src/components/Voluntario.js
@@ -20,27 +20,22 @@ function Voluntario() {
     };
 
     useEffect(() => {
-        const elefante = document.getElementById('hoverElefante');
-        const hoverSound = document.getElementById('hoverSound');
-    
-        const playSoundOnHover = () => {
-            hoverSound.play();
-        };
-    
-        const enableSound = () => {
-            if (elefante && hoverSound) {
-                elefante.addEventListener('mouseenter', playSoundOnHover);
-            }
-            document.removeEventListener('click', enableSound);
-        };
-    
-        document.addEventListener('click', enableSound);
-    
+        const button = document.getElementById('btnSendForm');
+        const clickSound = document.getElementById('clickSound');
+
+        if (button && clickSound) {
+            button.addEventListener('click', () => {
+                clickSound.play();
+            });
+        }
+
+        // Limpieza del evento cuando el componente se desmonte
         return () => {
-            if (elefante) {
-                elefante.removeEventListener('mouseenter', playSoundOnHover);
+            if (button) {
+                button.removeEventListener('click', () => {
+                    clickSound.play();
+                });
             }
-            document.removeEventListener('click', enableSound);
         };
     }, []);
 
