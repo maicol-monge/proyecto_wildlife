6bda7b628b94154162bb7626bb6be7baf86f4f8d
Maicol Monge <maicol.monge@catolica.edu.sv>
2024-10-19 22:51:51 -0600

    BD

--- src/Server.js
+++ src/Server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3001; // Cambia si es necesario
+const port = 3306; // Cambia si es necesario
 
 // Middleware
 app.use(cors());
@@ -12,29 +12,29 @@ app.use(bodyParser.json());
 
 // Conectar a la base de datos MySQL
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root', // Cambia por tu usuario de MySQL
-  password: '', // Cambia por tu contraseña
-  database: 'voluntarios_db' // Nombre de tu base de datos
+    host: 'localhost',
+    user: 'root', // Cambia por tu usuario de MySQL
+    password: '', // Cambia por tu contraseña
+    database: 'protect_our_wildlife' // Nombre de tu base de datos
 });
 
 db.connect(err => {
-  if (err) throw err;
-  console.log('Conectado a la base de datos');
+    if (err) throw err;
+    console.log('Conectado a la base de datos');
 });
 
 // Ruta para registrar un voluntario
 app.post('/register', (req, res) => {
-  const { nombre, apellido, edad, correo, tipoVoluntario, direccion } = req.body;
-  const sql = 'INSERT INTO voluntarios (nombre, apellido, edad, correo, tipoVoluntario, direccion) VALUES (?, ?, ?, ?, ?, ?)';
-  
-  db.query(sql, [nombre, apellido, edad, correo, tipoVoluntario, direccion], (err, result) => {
-    if (err) throw err;
-    res.send('Voluntario registrado con éxito');
-  });
+    const { nombre, apellido, edad, correo, tipoVoluntario, pais, direccion } = req.body;
+    const sql = 'INSERT INTO voluntarios (nombre, apellido, edad, correo, tipoVoluntario, pais, direccion) VALUES (?, ?, ?, ?, ?, ?, ?)';
+
+    db.query(sql, [nombre, apellido, edad, correo, tipoVoluntario, pais, direccion], (err, result) => {
+        if (err) throw err;
+        res.send('Voluntario registrado con éxito');
+    });
 });
 
 // Iniciar servidor
 app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
+    console.log(`Servidor escuchando en http://localhost:${port}`);
 });
